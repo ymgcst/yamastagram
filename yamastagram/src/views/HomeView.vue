cd86b8388edfe94450443ec31c7dba6a5aa48014
ymgcst <shota.yamaguchi@hopin.co.jp>
2022-03-12 23:14:01 +0900

    Add Vuetify

--- yamastagram/src/views/HomeView.vue
+++ yamastagram/src/views/HomeView.vue
@@ -1,27 +1,18 @@
 <template>
-  <div class="home">
-    <img class="homeImage" alt="pingu" src="../assets/pingu.jpg">
-    <HelloWorld msg="こんにちは"/>
-  </div>
+  <hello-world />
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from 'vue';
 
-@Options({
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
   components: {
     HelloWorld,
   },
-})
-export default class HomeView extends Vue {}
+});
 </script>
-
-<style scoped>
-.homeImage {
-  /*border-radius: 20px;*/
-  width: 100%;
-  max-width: 400px;
-  margin: 0px, 10%px;
-}
-</style>
