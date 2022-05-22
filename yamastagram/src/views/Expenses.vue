f531e2a5942ab627eff3bd522053c05a7ae2ce59
ymgcst <shota.yamaguchi@hopin.co.jp>
2022-05-22 23:22:18 +0900

    expenses modified

--- yamastagram/src/views/Expenses.vue
+++ yamastagram/src/views/Expenses.vue
@@ -1,27 +1,84 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <h1>Expenses</h1>
-      </v-col>
-      <v-col cols="4" v-for="i in 9" :key="i">
-        <v-btn>
-          {{ i }}
-        </v-btn>
-      </v-col>
-    </v-row>
+    <h1 style="text-align: center">Expenses</h1>
+
+    <div class="buttons">
+      <v-btn color="yellow" v-for="i in 9" :key="i">
+        {{ i }}
+      </v-btn>
+    </div>
+
+    <div style="text-align: center; margin: 0 0 10px">
+      <v-btn color="green" style="width: 300px" @click.stop="registExpenses">
+        register
+      </v-btn>
+    </div>
+
+    <div style="text-align: center; margin: 0 0 10px">
+      <v-btn color="green" style="width: 300px" @click.stop="clearExpenses">
+        clear
+      </v-btn>
+    </div>
+
+    <div style="text-align: center; margin: 0 0 10px">
+      <h3 v-for="bill in bills" :key="bill">{{bill.bill}}</h3>
+    </div>
+
   </v-container>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
+
+type Bill = {
+    bill: string;
+}
 
 @Options({
-  components: {
-  },
+  components: {},
 })
-export default class Expenses extends Vue {}
+
+export default class Expenses extends Vue {
+  // お勘定
+  private bills: Bill[] = [];
+
+  public created() {
+    this.getBills();
+  }
+
+  private getBills() {
+    let storageData = window.localStorage.getItem("testKey");
+    if (storageData == null) storageData = "[]";
+    this.bills = JSON.parse(storageData);
+  }
+
+  private registExpenses() {
+    let date = new Date().toString();
+    this.bills.push({bill: date});
+    window.localStorage.setItem("testKey", JSON.stringify(this.bills));
+    
+    console.log(this.bills);
+  }
+
+  private clearExpenses() {
+    window.localStorage.clear();
+    this.getBills();
+  }
+
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.buttons {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 300px;
+  margin: 0 auto 0;
+  button {
+    width: 30%;
+    margin: 0 0 10px;
+    font-size: 15px;
+  }
+}
 </style>
