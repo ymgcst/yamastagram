b56e7f6ed0f53d6e49c145897b0cc00bfaf619ba
ymgcst <shota.yamaguchi@hopin.co.jp>
2022-05-23 20:56:36 +0900

    ビルド先を変更

--- yamastagram/vue.config.js
+++ yamastagram/vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: './',
+  outputDir: '../../ymgcst.github.io/docs',
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
